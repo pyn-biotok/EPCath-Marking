a70c9d68af14f7cc4cc2fc6c96094c36cb7e7c2d
Yakov <pyn@biotok.ru>
2021-12-16 00:57:09 +0700

    Создан класс CatheterParam. В нем будут храниться данные о варианте исполнения катетера с их характеристиками

---
+++ CatheterParam.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPCath_Marking
+{
+    class CatheterParam
+    {
+        string curveType, catheterType, catheterDia, catheterContQTY, catheterContSpac, catheterIrrPort, catheterDistalLength;
+
+        public CatheterParam()
+        {
+            
+        }
+    }
+    
+}
